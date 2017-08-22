c7b10fb9174d22d0fa80403b8eb8e90c615cb0c2
cgoldsby <cgoldsby@gmail.com>
2017-08-22 07:47:23 -0400

    Present popover

---
+++ Custom Popover/Custom Popover/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Custom Popover
+//
+//  Created by Christopher Goldsby on 9/3/17.
+//  Copyright © 2017 Christopher Goldsby. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController, UIPopoverPresentationControllerDelegate {
+    
+    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var callToActionButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpView()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let popoverViewController = segue.destination
+        
+        // Override the presentation style on the iPhone so that the popover does not appear in fullscreen
+        popoverViewController.popoverPresentationController?.delegate = self
+        
+        // Given the orientation of the iPhone, present the popover either above or to the right of the label
+        popoverViewController.popoverPresentationController?.permittedArrowDirections = [.down, .left]
+        
+        // Define the size of the popover
+        popoverViewController.preferredContentSize = CGSize(width: 100, height: 100)
+        
+        // Present the popover from the label instead of the CTA button
+        popoverViewController.popoverPresentationController?.sourceRect = label.frame
+        popoverViewController.popoverPresentationController?.sourceView = label
+    }
+    
+    // MARK: - UIPopoverPresentationControllerDelegate
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .none
+    }
+    
+    // MARK: - Private
+    
+    private func setUpView() {
+        callToActionButton.layer.borderColor = UIColor.white.cgColor
+        callToActionButton.contentEdgeInsets = UIEdgeInsetsMake(5, 10, 5, 10)
+    }
+}
